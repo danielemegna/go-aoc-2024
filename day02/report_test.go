d4e046301f245ae8fcf1b37ef8afe2ac17a9d4ab
Daniele Megna <daniele.megna@qmates.tech>
2024-12-07 08:05:29 +0100

    Refactor in tests: better naming and order

--- day02/report_test.go
+++ day02/report_test.go
@@ -10,23 +10,22 @@ func TestSequentialIncreasingReportIsValid(t *testing.T) {
 	assert.True(t, report.IsValid())
 }
 
-func TestNotConstantIncreasingReportIsInvalid(t *testing.T) {
-	var report = Report{[]int{1, 3, 2}}
-	assert.False(t, report.IsValid())
-}
-
-func TestConstantReportIsNotValid(t *testing.T) {
-	var report = Report{[]int{2, 2, 2}}
-	assert.False(t, report.IsValid())
-}
-
-func TestReportWithTwoConstantValueInTheMiddleIsNotValid(t *testing.T) {
-	var report = Report{[]int{1, 2, 3, 3, 4}}
-	assert.False(t, report.IsValid())
-}
-
 func TestSequentialDecreasingReportIsValid(t *testing.T) {
 	var report = Report{[]int{4, 3, 2}}
 	assert.True(t, report.IsValid())
 }
 
+func TestNotIncreasingOrDecreasingReportIsInvalid(t *testing.T) {
+	var report = Report{[]int{1, 3, 2}}
+	assert.False(t, report.IsValid())
+}
+
+func TestConstantLevelValuesReportIsNotValid(t *testing.T) {
+	var report = Report{[]int{2, 2, 2}}
+	assert.False(t, report.IsValid())
+}
+
+func TestReportWithConstantLevelValuesInTheMiddleIsNotValid(t *testing.T) {
+	var report = Report{[]int{1, 2, 3, 3, 4}}
+	assert.False(t, report.IsValid())
+}
