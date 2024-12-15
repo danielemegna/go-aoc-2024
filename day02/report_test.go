3bff6dc9b53ed34d29c5a82cb9cd3f2353253265
Matteo Vaccari <mvaccari@thoughtworks.com>
2024-12-15 21:13:23 +0100

    Suggest how to use tabular tests to replace many individual tests

--- day02/report_test.go
+++ day02/report_test.go
@@ -5,123 +5,148 @@ import (
 	"testing"
 )
 
-func TestSequentialIncreasingReportIsSafe(t *testing.T) {
-	var report = Report{[]int{1, 2, 3}}
-	assert.True(t, report.IsSafe())
-}
-
-func TestSequentialDecreasingReportIsSafe(t *testing.T) {
-	var report = Report{[]int{4, 3, 2}}
-	assert.True(t, report.IsSafe())
-}
-
-func TestNotIncreasingOrDecreasingReportIsNotSafe(t *testing.T) {
-	var report = Report{[]int{1, 3, 2}}
-	assert.False(t, report.IsSafe())
-}
-
-func TestConstantLevelValuesReportIsNotSafe(t *testing.T) {
-	var report = Report{[]int{2, 2, 2}}
-	assert.False(t, report.IsSafe())
-}
-
-func TestReportWithConstantLevelValuesInTheMiddleIsNotSafe(t *testing.T) {
-	var report = Report{[]int{1, 2, 3, 3, 4}}
-	assert.False(t, report.IsSafe())
-}
-
-func TestReportWithAtMostThreeLevelDifferencesIsSafe(t *testing.T) {
-	var report = Report{[]int{1, 3, 6, 7, 9}}
-	assert.True(t, report.IsSafe())
-}
-
-func TestIncreasingReportWithMoreThanThreeLevelDifferenceIsNotSafe(t *testing.T) {
-	var report = Report{[]int{1, 2, 7, 8, 9}}
-	assert.False(t, report.IsSafe())
-}
-
-func TestDecreasingReportWithMoreThanThreeLevelDifferenceIsNotSafe(t *testing.T) {
-	var report = Report{[]int{9, 7, 6, 2, 1}}
-	assert.False(t, report.IsSafe())
-}
-
-func TestSafeReportToleratingStartingSingleBadLevel(t *testing.T) {
-	var reports = []Report{
-		{[]int{99, 1, 3, 5}},
-		{[]int{99, 7, 6, 5}},
-		{[]int{1, 6, 7, 8}},
-		{[]int{1, 7, 6, 5}},
-		{[]int{1, 1, 3, 5}},
-		{[]int{7, 7, 6, 5}},
-		{[]int{8, 6, 7, 8}},
-		{[]int{6, 7, 6, 5}},
+func Test_ReportSafety(t *testing.T) {
+	tests := []struct {
+		name         string
+		levels       []int
+		expectedSafe bool
+	}{
+		{
+			name:         "safe: sequential increasing report",
+			levels:       []int{1, 2, 3},
+			expectedSafe: true,
+		},
+		{
+			name:         "safe: sequential decreasing report",
+			levels:       []int{3, 2, 1},
+			expectedSafe: true,
+		},
+		{
+			name:         "safe: at most 3 level of distance increasing",
+			levels:       []int{1, 3, 6, 7, 9},
+			expectedSafe: true,
+		},
+		{
+			name:         "safe: at most 3 level of distance decreasing",
+			levels:       []int{9, 7, 4, 1},
+			expectedSafe: true,
+		},
+		{
+			name:         "unsafe: repeated level",
+			levels:       []int{1, 2, 2, 3},
+			expectedSafe: false,
+		},
+		{
+			name:         "unsafe: distance greater than 4, increasing",
+			levels:       []int{1, 2, 6, 7},
+			expectedSafe: false,
+		},
+		{
+			name:         "unsafe: distance greater than 4, decreasing",
+			levels:       []int{7, 6, 2, 1},
+			expectedSafe: false,
+		},
+		{
+			name:         "unsafe: non monotonic",
+			levels:       []int{1, 2, 3, 2},
+			expectedSafe: false,
+		},
 	}
-
-	for _, report := range reports {
-		assert.False(t, report.IsSafe(), report)
-		assert.True(t, report.IsSafeWithTolerance(), report)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			report := Report{test.levels}
+			assert.Equal(t, test.expectedSafe, report.IsSafe(), "IsSafe? ", test.levels)
+		})
 	}
 }
 
-func TestSafeReportToleratingSingleBadLevelInTheMiddle(t *testing.T) {
-	var reports = []Report{
-		{[]int{5, 1, 7, 8}},
-		{[]int{5, 6, 99, 8}},
-		{[]int{5, 6, 6, 8}},
-		{[]int{5, 6, 5, 8}},
-		{[]int{5, 4, 6, 7}},
-		{[]int{5, 4, 8, 9}},
+func Test_ReportRecoverability(t *testing.T) {
+	tests := []struct {
+		name                      string
+		levels                    []int
+		expectedSafeWithTolerance bool
+	}{
+		{
+			name:                      "single bad value at beginning: too large, sequence increasing",
+			levels:                    []int{99, 1, 3, 5},
+			expectedSafeWithTolerance: true,
+		},
+		{
+			name:                      "single bad value at beginning: too large, sequence decreasing",
+			levels:                    []int{99, 7, 6, 5},
+			expectedSafeWithTolerance: true,
+		},
+		{
+			name:                      "single bad value at beginning: too small, sequence decreasing",
+			levels:                    []int{1, 6, 7, 8},
+			expectedSafeWithTolerance: true,
+		},
+		{
+			name:                      "single bad value at beginning: repeated value",
+			levels:                    []int{1, 1, 3, 5},
+			expectedSafeWithTolerance: true,
+		},
+		{
+			name:                      "single bad value at beginning: decreasing in increasing sequence",
+			levels:                    []int{8, 6, 7, 8},
+			expectedSafeWithTolerance: true,
+		},
+		{
+			name:                      "single bad value at beginning: increasing in decreasing sequence",
+			levels:                    []int{2, 3, 2, 1},
+			expectedSafeWithTolerance: true,
+		},
 
-		{[]int{8, 1, 6, 5}},
-		{[]int{8, 7, 99, 5}},
-		{[]int{8, 7, 7, 5}},
-		{[]int{8, 7, 8, 5}},
-		{[]int{8, 9, 5, 4}},
+		{
+			name:                      "single bad value in the middle: too small",
+			levels:                    []int{5, 1, 7, 8},
+			expectedSafeWithTolerance: true,
+		},
+		{
+			name:                      "single bad value in the middle: too large",
+			levels:                    []int{5, 99, 7, 8},
+			expectedSafeWithTolerance: true,
+		},
+		{
+			name:                      "single bad value in the middle: repeated value",
+			levels:                    []int{5, 7, 7, 8},
+			expectedSafeWithTolerance: true,
+		},
+		{
+			name:                      "single bad value in the middle: decreasing in increasing sequence",
+			levels:                    []int{5, 4, 6, 7},
+			expectedSafeWithTolerance: true,
+		},
+		{
+			name:                      "single bad value in the end: too small",
+			levels:                    []int{7, 8, 9, 1},
+			expectedSafeWithTolerance: true,
+		},
+		{
+			name:                      "single bad value in the end: too large",
+			levels:                    []int{1, 2, 3, 99},
+			expectedSafeWithTolerance: true,
+		},
 
-		{[]int{1, 3, 2, 4, 5}},
-		{[]int{8, 6, 4, 4, 1}},
-		{[]int{27, 29, 25, 22, 19}},
-		{[]int{44, 47, 46, 47, 49}},
+		{
+			name:                      "unrecoverable despite single bad value",
+			levels:                    []int{1, 2, 99, 2},
+			expectedSafeWithTolerance: false,
+		},
+
+		{
+			name:                      "unrecoverable because of 2 bad values",
+			levels:                    []int{1, 2, 99, 3, 99},
+			expectedSafeWithTolerance: false,
+		},
 	}
-
-	for _, report := range reports {
-		assert.False(t, report.IsSafe(), report)
-		assert.True(t, report.IsSafeWithTolerance(), report)
-	}
-}
-
-func TestSafeReportToleratingSingleBadLevelAtTheEnd(t *testing.T) {
-	var reports = []Report{
-		{[]int{1, 3, 6, 1}},
-		{[]int{1, 3, 6, 10}},
-		{[]int{1, 3, 6, 3}},
-		{[]int{1, 3, 6, 6}},
-		{[]int{8, 7, 6, 10}},
-		{[]int{8, 7, 6, 1}},
-		{[]int{8, 7, 6, 7}},
-		{[]int{8, 7, 6, 6}},
-	}
-
-	for _, report := range reports {
-		assert.False(t, report.IsSafe(), report)
-		assert.True(t, report.IsSafeWithTolerance(), report)
-	}
-}
-
-func TestUnsafeReportRegardlessToleratingSingleBadLevel(t *testing.T) {
-	var reports = []Report{
-		{[]int{1, 2, 7, 8, 9}},
-		{[]int{9, 7, 6, 2, 1}},
-		{[]int{1, 2, 3, 2, 3}},
-		{[]int{99, 3, 4, 5, 99}},
-		{[]int{3, 99, 4, 5, 99}},
-		{[]int{99, 3, 4, 99, 5}},
-		{[]int{3, 99, 4, 99, 5}},
-	}
-
-	for _, report := range reports {
-		assert.False(t, report.IsSafe(), report)
-		assert.False(t, report.IsSafeWithTolerance(), report)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			report := Report{test.levels}
+			assert.False(t, report.IsSafe(), "IsSafe? ", report)
+			assert.Equal(t, test.expectedSafeWithTolerance, report.IsSafeWithTolerance(),
+				"SafeWithTolerange? ", test.levels)
+		})
 	}
 }
 
