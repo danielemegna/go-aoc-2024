3ba265869963234c8a2386a6f8955af22ae39381
Daniele Megna <daniele.megna@qmates.tech>
2024-12-18 07:55:39 +0100

    Simplify first part acceptance test

--- day03/main_test.go
+++ day03/main_test.go
@@ -3,16 +3,11 @@ package day03
 import (
 	"github.com/stretchr/testify/assert"
 	"os"
-	"strings"
 	"testing"
 )
 
-var PROVIDED_EXAMPLE_INPUT_LINES = []string{
-	"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))",
-}
-
 func TestSolveFirstPartWithProvidedExample(t *testing.T) {
-	var fileContent = simulateFileContent(PROVIDED_EXAMPLE_INPUT_LINES)
+	var fileContent = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 	assert.Equal(t, 2*4+5*5+11*8+8*5, SumOfInstructionsIn(fileContent))
 }
 
@@ -20,10 +15,6 @@ func TestSolveFirstPartWithFile(t *testing.T) {
 	var fileContent = readFileContent()
 	var actual = SumOfInstructionsIn(fileContent)
 	assert.Equal(t, 174561379, actual)
-}
-
-func simulateFileContent(inputLines []string) string {
-	return strings.Join(inputLines, "\n") + "\n"
 }
 
 func readFileContent() string {
