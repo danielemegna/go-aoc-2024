42ff484bbbbea7c04277b0dcf5f8f39ec2b6c969
Daniele Megna <megna.dany@gmail.com>
2024-12-19 09:01:32 +0100

    Starting day04 with green acceptance test and dummy green implementation

---
+++ day04/main.go
@@ -0,0 +1,5 @@
+package day04
+
+func CountXMasOccurences(fileContent string) int {
+	return 18
+}
---
+++ day04/main_test.go
@@ -0,0 +1,44 @@
+package day04
+
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"strings"
+	"testing"
+)
+
+var PROVIDED_EXAMPLE_INPUT_LINES = []string{
+	"MMMSXXMASM",
+	"MSAMXMSMSA",
+	"AMXSXMAAMM",
+	"MSAMASMSMX",
+	"XMASAMXAMM",
+	"XXAMMXXAMA",
+	"SMSMSASXSS",
+	"SAXAMASAAA",
+	"MAMMMXMMMM",
+	"MXMXAXMASX",
+}
+
+func TestSolveFirstPartWithProvidedExample(t *testing.T) {
+	var fileContent = simulateFileContent(PROVIDED_EXAMPLE_INPUT_LINES)
+	assert.Equal(t, 18, CountXMasOccurences(fileContent))
+}
+
+func TestSolveFirstPartWithFile(t *testing.T) {
+	t.Skip("WIP")
+	var fileContent = readFileContent()
+	assert.Equal(t, 999, CountXMasOccurences(fileContent))
+}
+
+func simulateFileContent(inputLines []string) string {
+	return strings.Join(inputLines, "\n") + "\n"
+}
+
+func readFileContent() string {
+	var fileBytes, err = os.ReadFile("input.txt")
+	if err != nil {
+		panic("Cannot find input.txt file to solve with file")
+	}
+	return string(fileBytes)
+}
