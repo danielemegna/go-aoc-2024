48c2fe1322dbc50097427798d9c7e69aed2c6dac
Daniele Megna <daniele.megna@qmates.tech>
2024-12-23 07:57:10 +0100

    Add bounds check in XMasOccurrencesAt(Coordinate)

--- day04/characters_map.go
+++ day04/characters_map.go
@@ -8,6 +8,10 @@ type Coordinate struct {
 }
 
 func (this CharactersMap) XMasOccurrencesAt(c Coordinate) int {
+	if this.IsOutOfBounds(c.Y) || this.IsOutOfBounds(c.X) {
+		return 0
+	}
+
 	if this[c.Y][c.X] != "X" {
 		return 0
 	}
@@ -15,10 +19,17 @@ func (this CharactersMap) XMasOccurrencesAt(c Coordinate) int {
 	var neededParts = []string{"M", "A", "S"}
 	for partIndex, neededPart := range neededParts {
 		var checkingX = c.X + partIndex + 1
+		if this.IsOutOfBounds(checkingX) {
+			return 0
+		}
 		if this[c.Y][checkingX] != neededPart {
 			return 0
 		}
 	}
 
 	return 1
+}
+
+func (this CharactersMap) IsOutOfBounds(i int) bool {
+	return i >= len(this)
 }
--- day04/characters_map_test.go
+++ day04/characters_map_test.go
@@ -40,3 +40,25 @@ func TestUncompleteXMasHorizontalOccurrence(t *testing.T) {
 
 	assert.Equal(t, 0, m.XMasOccurrencesAt(Coordinate{X: 0, Y: 1}))
 }
+
+func TestNoXMasOccurrencesOutOfMapBounds(t *testing.T) {
+	var m CharactersMap = CharactersMap{
+		{"X", "M", "A", "S"},
+		{".", ".", ".", "."},
+		{".", ".", ".", "."},
+		{".", ".", ".", "."},
+	}
+
+	assert.Equal(t, 0, m.XMasOccurrencesAt(Coordinate{X: 99, Y: 0}))
+	assert.Equal(t, 0, m.XMasOccurrencesAt(Coordinate{X: 0, Y: 99}))
+}
+
+func TestNoXMasOccurrencesInSmallMap(t *testing.T) {
+	var m CharactersMap = CharactersMap{
+		{"X", "M", "A"},
+		{".", ".", "."},
+		{".", ".", "."},
+	}
+
+	assert.Equal(t, 0, m.XMasOccurrencesAt(Coordinate{X: 0, Y: 0}))
+}
