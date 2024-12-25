3eb14d9e78ad6b71f9d5b3379ea050070add5585
Daniele Megna <daniele.megna@qmates.tech>
2024-12-25 12:29:15 +0100

    Run day04 AT with input file: first part solved! :tada:

--- day04/main_test.go
+++ day04/main_test.go
@@ -26,9 +26,8 @@ func TestSolveFirstPartWithProvidedExample(t *testing.T) {
 }
 
 func TestSolveFirstPartWithFile(t *testing.T) {
-	t.Skip("WIP")
 	var fileContent = readFileContent()
-	assert.Equal(t, 999, CountXMasOccurrences(fileContent))
+	assert.Equal(t, 2464, CountXMasOccurrences(fileContent))
 }
 
 func simulateFileContent(inputLines []string) string {
