4ae78d717194c852107eed9057edf7eb08d22abf
Daniele Megna <daniele.megna@qmates.tech>
2024-12-26 23:11:03 +0100

    Use Coordinate move-clone functions in every MapCursor.Increase()

--- day04/coordinate.go
+++ day04/coordinate.go
@@ -5,18 +5,11 @@ type Coordinate struct {
 	Y int
 }
 
-func (this Coordinate) NorthEast() Coordinate {
-	return Coordinate{this.X + 1, this.Y - 1}
-}
-
-func (this Coordinate) NorthOvest() Coordinate {
-	return Coordinate{this.X - 1, this.Y - 1}
-}
-
-func (this Coordinate) SouthEast() Coordinate {
-	return Coordinate{this.X + 1, this.Y + 1}
-}
-
-func (this Coordinate) SouthOvest() Coordinate {
-	return Coordinate{this.X - 1, this.Y + 1}
-}
+func (this Coordinate) North() Coordinate      { return Coordinate{this.X, this.Y - 1} }
+func (this Coordinate) South() Coordinate      { return Coordinate{this.X, this.Y + 1} }
+func (this Coordinate) East() Coordinate       { return Coordinate{this.X + 1, this.Y} }
+func (this Coordinate) West() Coordinate       { return Coordinate{this.X - 1, this.Y} }
+func (this Coordinate) NorthEast() Coordinate  { return Coordinate{this.X + 1, this.Y - 1} }
+func (this Coordinate) NorthOvest() Coordinate { return Coordinate{this.X - 1, this.Y - 1} }
+func (this Coordinate) SouthEast() Coordinate  { return Coordinate{this.X + 1, this.Y + 1} }
+func (this Coordinate) SouthOvest() Coordinate { return Coordinate{this.X - 1, this.Y + 1} }
--- day04/map_cursor.go
+++ day04/map_cursor.go
@@ -7,40 +7,40 @@ type MapCursor interface {
 
 type HorizontalMapCursor struct{ c Coordinate }
 
-func (this *HorizontalMapCursor) Increase()                { this.c.X++ }
+func (this *HorizontalMapCursor) Increase()                { this.c = this.c.East() }
 func (this *HorizontalMapCursor) ToCoordinate() Coordinate { return this.c }
 
 type VerticalMapCursor struct{ c Coordinate }
 
-func (this *VerticalMapCursor) Increase()                { this.c.Y++ }
+func (this *VerticalMapCursor) Increase()                { this.c = this.c.South() }
 func (this *VerticalMapCursor) ToCoordinate() Coordinate { return this.c }
 
 type InverseHorizontalMapCursor struct{ c Coordinate }
 
-func (this *InverseHorizontalMapCursor) Increase()                { this.c.X-- }
+func (this *InverseHorizontalMapCursor) Increase()                { this.c = this.c.West() }
 func (this *InverseHorizontalMapCursor) ToCoordinate() Coordinate { return this.c }
 
 type InverseVerticalMapCursor struct{ c Coordinate }
 
-func (this *InverseVerticalMapCursor) Increase()                { this.c.Y-- }
+func (this *InverseVerticalMapCursor) Increase()                { this.c = this.c.North() }
 func (this *InverseVerticalMapCursor) ToCoordinate() Coordinate { return this.c }
 
 type NorthDiagonalMapCursor struct{ c Coordinate }
 
-func (this *NorthDiagonalMapCursor) Increase()                { this.c.X++; this.c.Y-- }
+func (this *NorthDiagonalMapCursor) Increase()                { this.c = this.c.NorthEast() }
 func (this *NorthDiagonalMapCursor) ToCoordinate() Coordinate { return this.c }
 
 type SouthDiagonalMapCursor struct{ c Coordinate }
 
-func (this *SouthDiagonalMapCursor) Increase()                { this.c.X++; this.c.Y++ }
+func (this *SouthDiagonalMapCursor) Increase()                { this.c = this.c.SouthEast() }
 func (this *SouthDiagonalMapCursor) ToCoordinate() Coordinate { return this.c }
 
 type NorthInverseDiagonalMapCursor struct{ c Coordinate }
 
-func (this *NorthInverseDiagonalMapCursor) Increase()                { this.c.X--; this.c.Y-- }
+func (this *NorthInverseDiagonalMapCursor) Increase()                { this.c = this.c.NorthOvest() }
 func (this *NorthInverseDiagonalMapCursor) ToCoordinate() Coordinate { return this.c }
 
 type SouthInverseDiagonalMapCursor struct{ c Coordinate }
 
-func (this *SouthInverseDiagonalMapCursor) Increase()                { this.c.X--; this.c.Y++ }
+func (this *SouthInverseDiagonalMapCursor) Increase()                { this.c = this.c.SouthOvest() }
 func (this *SouthInverseDiagonalMapCursor) ToCoordinate() Coordinate { return this.c }
