9e64a432ae5dc51e8b49404b78fb3e9b3eb9373c
Daniele Megna <daniele.megna@qmates.tech>
2024-12-31 16:06:21 +0100

    Add more test cases for update IsValidFor(rules) tests

--- day05/update_validator_test.go
+++ day05/update_validator_test.go
@@ -2,10 +2,9 @@ package day05
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/assert"
 	"strconv"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestUpdateIsAlwaysValidWithoutRules(t *testing.T) {
@@ -34,6 +33,7 @@ func TestThreeNumbersInvalidUpdate(t *testing.T) {
 	var setOfPageRules = []PageOrderingRules{
 		{{before: 46, after: 19}},
 		{{before: 90, after: 46}},
+		{{before: 90, after: 19}},
 		{
 			{before: 1, after: 99},
 			{before: 90, after: 46},
@@ -44,7 +44,6 @@ func TestThreeNumbersInvalidUpdate(t *testing.T) {
 			{before: 2, after: 99},
 			{before: 46, after: 19},
 		},
-		{{before: 90, after: 19}},
 		{
 			{before: 19, after: 99},
 			{before: 46, after: 99},
@@ -60,5 +59,69 @@ func TestThreeNumbersInvalidUpdate(t *testing.T) {
 			assert.False(t, isValid, fmt.Sprintf("%v should not be valid for %v !", update, rules))
 		})
 	}
+}
 
+func TestThreeNumbersValidUpdate(t *testing.T) {
+	var update = PagesToProduceInTheUpdate{19, 46, 90}
+	var setOfPageRules = []PageOrderingRules{
+		{{before: 19, after: 46}},
+		{{before: 46, after: 90}},
+		{{before: 19, after: 90}},
+		{
+			{before: 19, after: 46},
+			{before: 46, after: 90},
+			{before: 19, after: 90},
+		},
+	}
+
+	for index, rules := range setOfPageRules {
+		t.Run("Rules #"+strconv.Itoa(index+1), func(t *testing.T) {
+			var isValid = update.IsValidFor(rules)
+			assert.True(t, isValid, fmt.Sprintf("%v should be valid for %v !", update, rules))
+		})
+	}
+}
+
+func TestFiveNumbersInvalidUpdate(t *testing.T) {
+	var update = PagesToProduceInTheUpdate{75, 97, 47, 61, 53}
+	var setOfPageRules = []PageOrderingRules{
+		{{before: 97, after: 75}},
+		{{before: 53, after: 61}},
+		{
+			{before: 47, after: 61},
+			{before: 53, after: 61},
+		},
+	}
+
+	for index, rules := range setOfPageRules {
+		t.Run("Rules #"+strconv.Itoa(index+1), func(t *testing.T) {
+			var isValid = update.IsValidFor(rules)
+			assert.False(t, isValid, fmt.Sprintf("%v should not be valid for %v !", update, rules))
+		})
+	}
+}
+
+func TestFiveNumbersValidUpdate(t *testing.T) {
+	var update = PagesToProduceInTheUpdate{75, 97, 47, 61, 53}
+	var setOfPageRules = []PageOrderingRules{
+		{{before: 75, after: 97}},
+		{{before: 75, after: 47}},
+		{{before: 75, after: 53}},
+		{{before: 61, after: 53}},
+		{
+			{before: 75, after: 97},
+			{before: 75, after: 47},
+			{before: 75, after: 53},
+			{before: 97, after: 53},
+			{before: 47, after: 61},
+			{before: 47, after: 53},
+		},
+	}
+
+	for index, rules := range setOfPageRules {
+		t.Run("Rules #"+strconv.Itoa(index+1), func(t *testing.T) {
+			var isValid = update.IsValidFor(rules)
+			assert.True(t, isValid, fmt.Sprintf("%v should be valid for %v !", update, rules))
+		})
+	}
 }
