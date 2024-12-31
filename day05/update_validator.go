835f63872d35ca76543489df798dd73378fbe0f6
Daniele Megna <daniele.megna@qmates.tech>
2024-12-31 15:48:32 +0100

    Simplify IsValidFor(rules)
    "before" slice should not really needed
    since we check from the begin the after

--- day05/update_validator.go
+++ day05/update_validator.go
@@ -4,7 +4,7 @@ import "slices"
 
 func (this PagesToProduceInTheUpdate) IsValidFor(rules PageOrderingRules) bool {
 	for i := 0; i < len(this)-1; i++ {
-		var before, currentPageNumber, after = this[:i], this[i], this[i+1:]
+		var currentPageNumber, after = this[i], this[i+1:]
 
 		var rulesForCurrentPage = rules.RulesFor(currentPageNumber)
 		if len(rulesForCurrentPage) == 0 {
@@ -12,16 +12,7 @@ func (this PagesToProduceInTheUpdate) IsValidFor(rules PageOrderingRules) bool {
 		}
 
 		var rule = rulesForCurrentPage[0]
-
 		if slices.Contains(after, rule.before) {
-			return false
-		}
-
-		if len(before) == 0 {
-			continue
-		}
-
-		if rule.after == before[0] {
 			return false
 		}
 
