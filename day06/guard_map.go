52a29a18dfd2a814bf37def261575ef613c3be99
Daniele Megna <megna.dany@gmail.com>
2025-01-17 07:12:18 +0100

    Extracted Coordinate stuff in dedicated file

--- day06/guard_map.go
+++ day06/guard_map.go
@@ -18,31 +18,6 @@ type Guard struct {
 	visitedPositions []Coordinate
 }
 
-type Coordinate struct {
-	x int
-	y int
-}
-
-func (this Coordinate) NextFor(direction Direction) Coordinate {
-	switch direction {
-	case North:
-		return this.North()
-	case South:
-		return this.South()
-	case East:
-		return this.East()
-	case West:
-		return this.West()
-	default:
-		panic(fmt.Sprintf("Unexpected Direction: %#v", direction))
-	}
-}
-
-func (this Coordinate) North() Coordinate { return Coordinate{this.x, this.y - 1} }
-func (this Coordinate) South() Coordinate { return Coordinate{this.x, this.y + 1} }
-func (this Coordinate) East() Coordinate  { return Coordinate{this.x + 1, this.y} }
-func (this Coordinate) West() Coordinate  { return Coordinate{this.x - 1, this.y} }
-
 type Direction int
 
 const (
