da727c033a6854bcf88108a21b3a3ea2bd4ecb9c
Daniele Megna <megna.dany@gmail.com>
2025-02-12 08:45:17 +0100

    Starting day07: acceptance test and dummy green implementation

---
+++ day07/main.go
@@ -0,0 +1,5 @@
+package day07
+
+func TotalCalibrationResultOfPossiblyTrueEquations(fileContent string) int {
+	return 292 + 190 + 3267
+}
---
+++ day07/main_test.go
@@ -0,0 +1,43 @@
+package day07
+
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"strings"
+	"testing"
+)
+
+var PROVIDED_EXAMPLE_INPUT_LINES = []string{
+	"190: 10 19",
+	"3267: 81 40 27",
+	"83: 17 5",
+	"156: 15 6",
+	"7290: 6 8 6 15",
+	"161011: 16 10 13",
+	"192: 17 8 14",
+	"21037: 9 7 18 13",
+	"292: 11 6 16 20",
+}
+
+func TestSolveFirstPartWithProvidedExample(t *testing.T) {
+	var fileContent = simulateFileContent(PROVIDED_EXAMPLE_INPUT_LINES)
+	assert.Equal(t, 292 + +190 + 3267, TotalCalibrationResultOfPossiblyTrueEquations(fileContent))
+}
+
+func TestSolveFirstPartWithFile(t *testing.T) {
+	t.Skip("WIP")
+	var fileContent = readFileContent()
+	assert.Equal(t, -1, TotalCalibrationResultOfPossiblyTrueEquations(fileContent))
+}
+
+func simulateFileContent(inputLines []string) string {
+	return strings.Join(inputLines, "\n") + "\n"
+}
+
+func readFileContent() string {
+	var fileBytes, err = os.ReadFile("input.txt")
+	if err != nil {
+		panic("Cannot find input.txt file to solve with file")
+	}
+	return string(fileBytes)
+}
