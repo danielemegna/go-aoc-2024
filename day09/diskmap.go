97d07e55773b32ba0bf69117e4be8d0d997224d8
Daniele Megna <megna.dany@gmail.com>
2025-03-05 06:50:18 +0100

    Working on DenseDiskMap ToExpandedDiskMap

--- day09/diskmap.go
+++ day09/diskmap.go
@@ -19,5 +19,13 @@ func ParseDenseDiskMap(diskMapString string) DenseDiskMap {
 }
 
 func ToExpandedDiskMap(denseDiskMap DenseDiskMap) ExpandedDiskMap {
-	return ExpandedDiskMap{data: []int{0, 0, 0}}
+	return ExpandedDiskMap{data: repeatInSlice(0, denseDiskMap.data[0])}
+}
+
+func repeatInSlice(value int, length int) []int {
+	var diskMapData = []int{}
+	for i := 0; i < length; i++ {
+		diskMapData = append(diskMapData, value)
+	}
+	return diskMapData
 }
