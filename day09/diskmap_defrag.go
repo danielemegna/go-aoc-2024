0a64a80bbd9aa2d0ceb943727c551100871d19ab
Daniele Megna <megna.dany@gmail.com>
2025-03-07 15:30:10 +0100

    Implement and test Defrag(ExpandedDiskMap) function

--- day09/diskmap_defrag.go
+++ day09/diskmap_defrag.go
@@ -1,8 +1,34 @@
 package day09
 
+import (
+	"github.com/samber/lo"
+)
+
 func Defrag(diskMap ExpandedDiskMap) ExpandedDiskMap {
-	// WIP
-	return ExpandedDiskMap{data: []int{
-		0, 2, 2, 1, 1, 1, 2, 2, 2, -1, -1, -1, -1, -1, -1,
-	}}
+	var newData = diskMap.data
+
+	for {
+		var found bool
+		var firstEmptySpaceIndex int
+		var lastNotEmptyDigitIndex int
+
+		_, firstEmptySpaceIndex, found = lo.FindIndexOf(newData, func(digit int) bool { return digit == -1 })
+		if !found {
+			break
+		}
+
+		_, lastNotEmptyDigitIndex, found = lo.FindLastIndexOf(newData, func(digit int) bool { return digit != -1 })
+		if !found {
+			break
+		}
+
+		if firstEmptySpaceIndex >= lastNotEmptyDigitIndex {
+			break
+		}
+
+		newData[firstEmptySpaceIndex] = newData[lastNotEmptyDigitIndex]
+		newData[lastNotEmptyDigitIndex] = -1
+	}
+
+	return ExpandedDiskMap{data: newData}
 }
--- day09/diskmap_defrag_test.go
+++ day09/diskmap_defrag_test.go
@@ -17,3 +17,31 @@ func TestDefragSimpleMap(t *testing.T) {
 	}}
 	assert.Equal(t, expected, actual)
 }
+
+func TestDefragProvidedExampleMap(t *testing.T) {
+	var diskMap = ExpandedDiskMap{data: []int{
+		0, 0, -1, -1, -1, 1, 1, 1, -1, -1, -1, 2, -1, -1, -1,
+		3, 3, 3, -1, 4, 4, -1, 5, 5, 5, 5, -1, 6, 6, 6, 6, -1,
+		7, 7, 7, -1, 8, 8, 8, 8, 9, 9,
+	}}
+
+	var actual = Defrag(diskMap)
+
+	var expected = ExpandedDiskMap{data: []int{
+		0, 0, 9, 9, 8, 1, 1, 1, 8, 8, 8, 2, 7, 7, 7, 3, 3, 3, 6, 4, 4, 6, 5, 5, 5, 5, 6, 6,
+		-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+	}}
+	assert.Equal(t, expected, actual)
+}
+
+func TestDefragSingleDigitMapDoesNothing(t *testing.T) {
+	var actual = Defrag(ExpandedDiskMap{data: []int{0}})
+	var expected = ExpandedDiskMap{data: []int{0}}
+	assert.Equal(t, expected, actual)
+}
+
+func TestDefragMapWithoutEmptySpacesDoesNothing(t *testing.T) {
+	var actual = Defrag(ExpandedDiskMap{data: []int{0, 0, 0, 1, 1, 2, 3, 3, 4, 4, 4}})
+	var expected = ExpandedDiskMap{data: []int{0, 0, 0, 1, 1, 2, 3, 3, 4, 4, 4}}
+	assert.Equal(t, expected, actual)
+}
