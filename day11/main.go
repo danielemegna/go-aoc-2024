31ebcd1b7004ea893cb6f716eb5338fec71f9ec4
Daniele Megna <megna.dany@gmail.com>
2025-04-28 08:19:26 +0200

    Integrating Stone in main
    to be continued ...

--- day11/main.go
+++ day11/main.go
@@ -1,5 +1,21 @@
 package day11
 
+import (
+	"github.com/samber/lo"
+	"strconv"
+	"strings"
+)
+
 func StonesCountAfterTwentyfiveBlinks(fileContent string) int {
+	var _ = lo.Map(
+		strings.Split(fileContent, " "),
+		func(numberString string, index int) Stone {
+			var engravedNumber, _ = strconv.ParseInt(numberString, 10, 64)
+			return Stone{engravedNumber: engravedNumber}
+		},
+	)
+
+	// TODO continue ...
+
 	return 55312
 }
