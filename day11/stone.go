e7edb2167a2afbb754076e574c0db76cbc19fdd5
Daniele Megna <megna.dany@gmail.com>
2025-04-28 07:39:22 +0200

    Developing Stone#OnBlink function

--- day11/stone.go
+++ day11/stone.go
@@ -1,9 +1,21 @@
 package day11
 
+import "strconv"
+
 type Stone struct {
 	engravedNumber int64
 }
 
-func (this Stone) OnBlink() []Stone {
-	return []Stone{Stone{engravedNumber: 1}}
+func (this Stone) OnBlink() (Stone, *Stone) {
+	if this.engravedNumber == 0 {
+		return Stone{engravedNumber: 1}, nil
+	}
+
+	var engravedNumberString = strconv.Itoa(int(this.engravedNumber))
+	var length = len(engravedNumberString)
+	var lengthHalf = length / 2
+
+	var left, _ = strconv.ParseInt(engravedNumberString[:lengthHalf], 10, 64)
+	var right, _ = strconv.ParseInt(engravedNumberString[lengthHalf:], 10, 64)
+	return Stone{engravedNumber: left}, &Stone{engravedNumber: right}
 }
--- day11/stone_test.go
+++ day11/stone_test.go
@@ -2,16 +2,47 @@ package day11
 
 import (
 	"github.com/stretchr/testify/assert"
+	"strconv"
 	"testing"
 )
 
-func TestStoneZeroChangeToOneOnBlink(t *testing.T) {
+func TestZeroEngravedStoneBlink(t *testing.T) {
 	var stone = Stone{engravedNumber: 0}
 
-	var stonesAfterBlink = stone.OnBlink()
+	var leftStone, rightStone = stone.OnBlink()
 
-	assert.Len(t, stonesAfterBlink, 1)
-	assert.Equal(t, Stone{engravedNumber: 1}, stonesAfterBlink[0])
+	assert.Equal(t, Stone{engravedNumber: 1}, leftStone)
+	assert.Nil(t, rightStone)
 }
 
+func TestEvenNumberOfDigitsEngravedStoneBlink(t *testing.T) {
+	var testCases = []struct {
+		engravedNumber int64
+		expectedLeft   int64
+		expectedRight  int64
+	}{
+		{2024, 20, 24},
+		{96, 9, 6},
+		{83794560, 8379, 4560},
+	}
 
+	for index, testCase := range testCases {
+		t.Run("Test case #"+strconv.Itoa(index+1), func(t *testing.T) {
+			var stone = Stone{engravedNumber: testCase.engravedNumber}
+
+			var leftStone, rightStone = stone.OnBlink()
+
+			assert.Equal(t, Stone{engravedNumber: testCase.expectedLeft}, leftStone)
+			assert.Equal(t, Stone{engravedNumber: testCase.expectedRight}, *rightStone)
+		})
+	}
+}
+
+func TestEvenNumberOfDigitsDoNotKeepExtraLeadingZeroes(t *testing.T) {
+	var stone = Stone{engravedNumber: 1001}
+
+	var leftStone, rightStone = stone.OnBlink()
+
+	assert.Equal(t, Stone{engravedNumber: 10}, leftStone)
+	assert.Equal(t, Stone{engravedNumber: 1}, *rightStone)
+}
