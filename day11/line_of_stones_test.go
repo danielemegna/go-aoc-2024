47bb0fec0f4e2a9f81161f91988647e0e9998f52
Daniele Megna <megna.dany@gmail.com>
2025-05-09 10:52:25 +0200

    Complete LineOfStones tests with a mixed duplicated stones case

--- day11/line_of_stones_test.go
+++ day11/line_of_stones_test.go
@@ -10,28 +10,45 @@ func TestLineOfZeroStonesOnBlink(t *testing.T) {
 
 	var actual = stones.OnBlink()
 
-	var expected = LineOfStones{1:3}
+	var expected = LineOfStones{1: 3}
 	assert.Equal(t, expected, actual)
 }
 
 func TestLineOfOddDigitsNumbersStonesOnBlink(t *testing.T) {
-	var stones = LineOfStones{1:1, 9:1, 146:1}
-
-	var actual = stones.OnBlink()
-
-	var expected = LineOfStones{2024:1, 9 * 2024:1, 146 * 2024:1}
-	assert.Equal(t, expected, actual)
-}
-
-func TestLineOfEvenDigitsNumbersStonesOnBlink(t *testing.T) {
-	var stones = LineOfStones{10:1, 46:1, 2823:1}
+	var stones = LineOfStones{1: 1, 9: 1, 146: 1}
 
 	var actual = stones.OnBlink()
 
 	var expected = LineOfStones{
-		1:1, 0:1,
-		4:1, 6:1,
-		28:1, 23:1,
+		2024:       1,
+		9 * 2024:   1,
+		146 * 2024: 1,
+	}
+	assert.Equal(t, expected, actual)
+}
+
+func TestLineOfEvenDigitsNumbersStonesOnBlink(t *testing.T) {
+	var stones = LineOfStones{10: 1, 46: 1, 2823: 1}
+
+	var actual = stones.OnBlink()
+
+	var expected = LineOfStones{
+		1: 1, 0: 1,
+		4: 1, 6: 1,
+		28: 1, 23: 1,
+	}
+	assert.Equal(t, expected, actual)
+}
+
+func TestPreserveDuplicatedStonesOnBlink(t *testing.T) {
+	var stones = LineOfStones{10: 2, 287: 3, 9871: 4}
+
+	var actual = stones.OnBlink()
+
+	var expected = LineOfStones{
+		1: 2, 0: 2,
+		287 * 2024: 3,
+		98: 4, 71: 4,
 	}
 	assert.Equal(t, expected, actual)
 }
