a2049c54a1da5c5cc7c0081f2df8bb7891f620f3
Daniele Megna <megna.dany@gmail.com>
2025-05-09 11:38:42 +0200

    Some refactor in Stone

--- day11/stone.go
+++ day11/stone.go
@@ -11,15 +11,30 @@ func (this Stone) OnBlink() (Stone, *Stone) {
 		return 1, nil
 	}
 
-	var engravedNumberString = strconv.Itoa(int(this))
-	var numberOfDigits = len(engravedNumberString)
+	var numberOfDigits = len(this.toString())
 	if numberOfDigits%2 != 0 {
 		return this * 2024, nil
 	}
 
-	var halfNumberOfDigits = numberOfDigits / 2
-	var left, _ = strconv.Atoi(engravedNumberString[:halfNumberOfDigits])
-	var right, _ = strconv.Atoi(engravedNumberString[halfNumberOfDigits:])
-	var rightStone = Stone(right)
-	return Stone(left), &rightStone
+	var leftStone = this.getLeftHalfStone()
+	var rightStone = this.getRightHalfStone()
+	return leftStone, &rightStone
+}
+
+func (this Stone) getLeftHalfStone() Stone {
+	var engravedNumberString = this.toString()
+	var halfNumberOfDigits = len(engravedNumberString) / 2
+	var newStoneNumber, _ = strconv.Atoi(engravedNumberString[:halfNumberOfDigits])
+	return Stone(newStoneNumber)
+}
+
+func (this Stone) getRightHalfStone() Stone {
+	var engravedNumberString = this.toString()
+	var halfNumberOfDigits = len(engravedNumberString) / 2
+	var newStone, _ = strconv.Atoi(engravedNumberString[halfNumberOfDigits:])
+	return Stone(newStone)
+}
+
+func (this Stone) toString() string {
+	return strconv.Itoa(int(this))
 }
