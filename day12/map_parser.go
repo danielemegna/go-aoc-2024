0d90c0673a0634b842926f534c3aa561de349a26
Daniele Megna <megna.dany@gmail.com>
2025-05-19 08:57:11 +0200

    More refactor in map_parser

--- day12/map_parser.go
+++ day12/map_parser.go
@@ -10,12 +10,14 @@ type Coordinate struct {
 	Y int
 }
 
+type RawGardenMap = []string
+
 func ParseGardenMap(fileContent string) GardenMap {
 	var visitedCoordinates = []Coordinate{}
 	var result = GardenMap{}
 
-	var rows = rowsFrom(fileContent)
-	for y, row := range rows {
+	var rawMap = rawGardenMapFrom(fileContent)
+	for y, row := range rawMap {
 		for x, char := range row {
 			var coordinate = Coordinate{x, y}
 			if slices.Contains(visitedCoordinates, coordinate) {
@@ -23,7 +25,7 @@ func ParseGardenMap(fileContent string) GardenMap {
 			}
 
 			var partialRegion = GardenRegion{plant: char, area: 0, perimeter: 0}
-			var gardenRegion = completeRegionVisit(partialRegion, coordinate, rows, &visitedCoordinates)
+			var gardenRegion = completeRegionVisit(partialRegion, coordinate, rawMap, &visitedCoordinates)
 			result = append(result, gardenRegion)
 		}
 	}
@@ -33,18 +35,18 @@ func ParseGardenMap(fileContent string) GardenMap {
 func completeRegionVisit(
 	partialRegion GardenRegion,
 	currentCoordinate Coordinate,
-	rows []string,
+	rawMap RawGardenMap,
 	visitedCoordinates *[]Coordinate,
 ) GardenRegion {
 	*visitedCoordinates = append(*visitedCoordinates, currentCoordinate)
 	partialRegion.area++
 
 	var currentCoordinatePerimeter = 0
-	var closeCoordinates = closePlantsFor(currentCoordinate, rows)
+	var closeCoordinates = closePlantsFor(currentCoordinate, rawMap)
 	currentCoordinatePerimeter += 4 - len(closeCoordinates)
 
 	for _, closeCoordinate := range closeCoordinates {
-		if !samePlantIn(currentCoordinate, closeCoordinate, rows) {
+		if !samePlantIn(currentCoordinate, closeCoordinate, rawMap) {
 			currentCoordinatePerimeter++
 			continue
 		}
@@ -56,7 +58,7 @@ func completeRegionVisit(
 		partialRegion = completeRegionVisit(
 			partialRegion,
 			closeCoordinate,
-			rows,
+			rawMap,
 			visitedCoordinates,
 		)
 	}
@@ -65,29 +67,29 @@ func completeRegionVisit(
 	return partialRegion
 }
 
-func samePlantIn(c1 Coordinate, c2 Coordinate, rows []string) bool {
-	return rows[c1.Y][c1.X] == rows[c2.Y][c2.X]
+func samePlantIn(c1 Coordinate, c2 Coordinate, rawMap RawGardenMap) bool {
+	return rawMap[c1.Y][c1.X] == rawMap[c2.Y][c2.X]
 }
 
-func closePlantsFor(c Coordinate, rows []string) []Coordinate {
-	var x, y = c.X, c.Y
+func closePlantsFor(c Coordinate, rawMap RawGardenMap) []Coordinate {
+	// check performance initializing with the four coordinates and applying a filter later
 	var closeCoordinates = []Coordinate{}
-	if x > 0 {
-		closeCoordinates = append(closeCoordinates, Coordinate{x - 1, y})
+	if c.X > 0 {
+		closeCoordinates = append(closeCoordinates, Coordinate{c.X - 1, c.Y})
 	}
-	if y > 0 {
-		closeCoordinates = append(closeCoordinates, Coordinate{x, y - 1})
+	if c.Y > 0 {
+		closeCoordinates = append(closeCoordinates, Coordinate{c.X, c.Y - 1})
 	}
-	if x < len(rows[y])-1 {
-		closeCoordinates = append(closeCoordinates, Coordinate{x + 1, y})
+	if c.X < len(rawMap[c.Y])-1 {
+		closeCoordinates = append(closeCoordinates, Coordinate{c.X + 1, c.Y})
 	}
-	if y < len(rows)-1 {
-		closeCoordinates = append(closeCoordinates, Coordinate{x, y + 1})
+	if c.Y < len(rawMap)-1 {
+		closeCoordinates = append(closeCoordinates, Coordinate{c.X, c.Y + 1})
 	}
 	return closeCoordinates
 }
 
-func rowsFrom(input string) []string {
+func rawGardenMapFrom(input string) RawGardenMap {
 	var rows = strings.Split(input, "\n")
 	rows = rows[:len(rows)-1]
 	return rows
