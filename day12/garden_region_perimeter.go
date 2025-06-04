66971e123d384ad15fc653d79cf7af631daf9426
Daniele Megna <megna.dany@gmail.com>
2025-06-04 07:55:32 +0200

    Two close elements should not increase side count in GardenRegionPerimeter
    GardenRegionPerimeter should expose only NumberOfSides and Length

--- day12/garden_region_perimeter.go
+++ day12/garden_region_perimeter.go
@@ -1,11 +1,77 @@
 package day12
 
+import "slices"
+
 type GardenRegionPerimeter struct {
-	coordinates []Coordinate
-	sides       int
+	borders []Border
+	sides   int
+}
+
+type Border struct {
+	direction  CardinalDirection
+	coordinate Coordinate
+}
+
+type CardinalDirection int
+
+const (
+	NORTH CardinalDirection = iota
+	SOUTH
+	EAST
+	WEST
+)
+
+func (this GardenRegionPerimeter) Length() int {
+	return len(this.borders)
+}
+
+func (this GardenRegionPerimeter) NumberOfSides() int {
+	return this.sides
 }
 
 func (this *GardenRegionPerimeter) Add(outsideCoordinate Coordinate, insideCoordinate Coordinate) {
-	this.coordinates = append(this.coordinates, outsideCoordinate)
-	this.sides++
+	var direction = cardinalDirectionFor(outsideCoordinate, insideCoordinate)
+	var border = Border{direction: direction, coordinate: insideCoordinate}
+	this.borders = append(this.borders, border)
+
+	if !this.isSideAlreadyCounted(border) {
+		this.sides++
+	}
+}
+
+func (this GardenRegionPerimeter) isSideAlreadyCounted(border Border) bool {
+	switch border.direction {
+	case EAST:
+		fallthrough
+	case NORTH:
+		fallthrough
+	case WEST:
+		fallthrough
+	case SOUTH:
+		if (this.contains(
+			Border{
+				direction:  border.direction,
+				coordinate: Coordinate{X: border.coordinate.X - 1, Y: border.coordinate.Y},
+			})) {
+			return true
+		}
+		if (this.contains(
+			Border{
+				direction:  border.direction,
+				coordinate: Coordinate{X: border.coordinate.X + 1, Y: border.coordinate.Y},
+			})) {
+			return true
+		}
+		fallthrough
+	default:
+		return false
+	}
+}
+
+func (this GardenRegionPerimeter) contains(border Border) bool {
+	return slices.Contains(this.borders, border)
+}
+
+func cardinalDirectionFor(outsideCoordinate, insideCoordinate Coordinate) CardinalDirection {
+	return SOUTH
 }
--- day12/garden_region_perimeter_test.go
+++ day12/garden_region_perimeter_test.go
@@ -12,8 +12,8 @@ func TestAddSingleElement(t *testing.T) {
 	var insideCoordinate = Coordinate{X: 1, Y: 3}
 	perimeter.Add(outsideCoordinate, insideCoordinate)
 
-	assert.Equal(t, 1, perimeter.sides)
-	assert.Equal(t, []Coordinate{{X: 1, Y: 2}}, perimeter.coordinates)
+	assert.Equal(t, 1, perimeter.NumberOfSides())
+	assert.Equal(t, 1, perimeter.Length())
 }
 
 func TestSomeFarElements(t *testing.T) {
@@ -22,16 +22,22 @@ func TestSomeFarElements(t *testing.T) {
 	perimeter.Add(Coordinate{X: 1, Y: 2}, Coordinate{X: 1, Y: 3})
 	perimeter.Add(Coordinate{X: 4, Y: 5}, Coordinate{X: 3, Y: 5})
 
-	assert.Equal(t, 2, perimeter.sides)
-	assert.Equal(t, []Coordinate{{X: 1, Y: 2}, {X: 4, Y: 5}}, perimeter.coordinates)
+	assert.Equal(t, 2, perimeter.NumberOfSides())
+	assert.Equal(t, 2, perimeter.Length())
 }
 
 func TestTwoCloseElementsShouldNotIncreaseSideCount(t *testing.T) {
 	var perimeter = GardenRegionPerimeter{}
 
 	perimeter.Add(Coordinate{X: 1, Y: 2}, Coordinate{X: 1, Y: 3})
-	perimeter.Add(Coordinate{X: 2, Y: 2}, Coordinate{X: 2, Y: 3})
+	assert.Equal(t, 1, perimeter.Length())
+	assert.Equal(t, 1, perimeter.NumberOfSides())
 
-	// assert.Equal(t, 1, perimeter.sides) WIP
-	assert.Equal(t, []Coordinate{{X: 1, Y: 2}, {X: 2, Y: 2}}, perimeter.coordinates)
+	perimeter.Add(Coordinate{X: 2, Y: 2}, Coordinate{X: 2, Y: 3})
+	assert.Equal(t, 2, perimeter.Length())
+	assert.Equal(t, 1, perimeter.NumberOfSides())
+
+	perimeter.Add(Coordinate{X: 0, Y: 2}, Coordinate{X: 0, Y: 3})
+	assert.Equal(t, 3, perimeter.Length())
+	assert.Equal(t, 1, perimeter.NumberOfSides())
 }
