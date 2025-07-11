4b347c2aed2d6d2677526fe0494866c50d52b79f
Daniele Megna <megna.dany@gmail.com>
2025-07-11 07:55:15 +0200

    Cover HowToWinThePrize with some other tests

--- day13/claw_machine_test.go
+++ day13/claw_machine_test.go
@@ -30,3 +30,29 @@ func TestClawMachineWithPrizeSolution(t *testing.T) {
 	assert.Equal(t, 80, buttonAPressCount)
 	assert.Equal(t, 40, buttonBPressCount)
 }
+
+func TestAnotherClawMachineWithPrizeSolution(t *testing.T) {
+	var machine = ClawMachine{
+		buttonA:         Coordinate{X: 17, Y: 86},
+		buttonB:         Coordinate{X: 84, Y: 37},
+		prizeCoordinate: Coordinate{X: 7870, Y: 6450},
+	}
+
+	var buttonAPressCount, buttonBPressCount = machine.HowToWinThePrize()
+
+	assert.Equal(t, 38, buttonAPressCount)
+	assert.Equal(t, 86, buttonBPressCount)
+}
+
+func TestAnotherClawMachineWithoutPrizeSolution(t *testing.T) {
+	var machine = ClawMachine{
+		buttonA:         Coordinate{X: 69, Y: 23},
+		buttonB:         Coordinate{X: 27, Y: 71},
+		prizeCoordinate: Coordinate{X: 18641, Y: 10279},
+	}
+
+	var buttonAPressCount, buttonBPressCount = machine.HowToWinThePrize()
+
+	assert.Equal(t, -1, buttonAPressCount)
+	assert.Equal(t, -1, buttonBPressCount)
+}
