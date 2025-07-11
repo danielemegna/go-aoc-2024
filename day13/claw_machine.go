a7319856369e1091939504be9d741c44cb96069e
Daniele Megna <megna.dany@gmail.com>
2025-07-11 07:48:43 +0200

    First draft of HowToWinThePrize reusing the FunzioneDelCuoco !

--- day13/claw_machine.go
+++ day13/claw_machine.go
@@ -14,10 +14,36 @@ type Coordinate struct {
 }
 
 func (this ClawMachine) HowToWinThePrize() (int, int) {
-	// WIP !
-	if(this.prizeCoordinate.X == 8400) {
-		return 80, 40
+
+	var guessBPressCount = this.prizeCoordinate.X / this.buttonB.X
+
+	for {
+		var differenza = this.prizeCoordinate.X - (this.buttonB.X * guessBPressCount)
+
+		var resto = differenza % this.buttonA.X
+		if resto == 0 {
+			var buttonAPressCount = differenza / this.buttonA.X
+			var buttonBPressCount = guessBPressCount
+
+			var y = (buttonAPressCount * this.buttonA.Y) + (buttonBPressCount * this.buttonB.Y)
+			if y == this.prizeCoordinate.Y {
+				return buttonAPressCount, buttonBPressCount
+			}
+
+			guessBPressCount--
+			if guessBPressCount < 0 {
+				break
+			}
+
+			continue
+		}
+
+		guessBPressCount--
+		if guessBPressCount < 0 {
+			break
+		}
 	}
 
 	return -1, -1
+
 }
