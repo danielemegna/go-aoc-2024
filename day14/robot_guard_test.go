401106230ac3458782d9ed88e5782bf656c88f98
Daniele Megna <megna.dany@gmail.com>
2025-07-23 08:51:50 +0200

    Start RobotGuard test driven development !

---
+++ day14/robot_guard_test.go
@@ -0,0 +1,33 @@
+package day14
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var SPACE_SIZE = SpaceSize{width: 11, height: 7}
+
+func TestChangePositionAfterOneSecondTick(t *testing.T) {
+	var robot = RobotGuard{
+		position: Position{x: 2, y: 2},
+		velocity: Velocity{horizontal: 1, vertical: 1},
+	}
+
+	robot.OneSecondTick(SPACE_SIZE)
+
+	var expectedNewPosition = Position{x: 2 + 1, y: 2 + 1}
+	assert.Equal(t, expectedNewPosition, robot.position)
+}
+
+func TestProvidedExampleRobotChangePosition(t *testing.T) {
+	t.Skip("WIP")
+	var robot = RobotGuard{
+		position: Position{x: 2, y: 4},
+		velocity: Velocity{horizontal: 2, vertical: -3},
+	}
+
+	robot.OneSecondTick(SPACE_SIZE)
+
+	var expectedNewPosition = Position{x: 4, y: 1}
+	assert.Equal(t, expectedNewPosition, robot.position)
+}
