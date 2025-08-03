251b5bfdd81222add45d73b647eb21cf5169e8d5
Daniele Megna <megna.dany@gmail.com>
2025-08-03 12:27:14 +0200

    Develop a PrintTree function in `day14/main.go` file
    use `go run day14/run/run.go` to run it

--- day14/main.go
+++ day14/main.go
@@ -1,5 +1,10 @@
 package day14
 
+import (
+	"fmt"
+	"strings"
+)
+
 func SafetyFactorAfter100Seconds(fileContent string, spaceSize SpaceSize) int {
 	var robots = ParseRobotLines(fileContent)
 	var space = Space{size: spaceSize, guards: robots}
@@ -7,4 +12,32 @@ func SafetyFactorAfter100Seconds(fileContent string, spaceSize SpaceSize) int {
 	space.AfterSeconds(100)
 
 	return space.GetSafetyFactor()
+}
+
+// run this with `go run day14/run/run.go`
+func PrintTree() {
+	fmt.Println("========= Hello, World!")
+	var space = buildSpace()
+	fmt.Printf("%+v\n", space)
+}
+
+func buildSpace() Space {
+	var providedExampleInputLines = []string{
+		"p=0,4 v=3,-3",
+		"p=6,3 v=-1,-3",
+		"p=10,3 v=-1,2",
+		"p=2,0 v=2,-1",
+		"p=0,0 v=1,3",
+		"p=3,0 v=-2,-2",
+		"p=7,6 v=-1,-3",
+		"p=3,0 v=-1,-2",
+		"p=9,3 v=2,3",
+		"p=7,3 v=-1,2",
+		"p=2,4 v=2,-3",
+		"p=9,5 v=-3,-3",
+	}
+	var fileContent = strings.Join(providedExampleInputLines, "\n") + "\n"
+	var spaceSize = SpaceSize{width: 11, height: 7}
+	var robots = ParseRobotLines(fileContent)
+	return Space{size: spaceSize, guards: robots}
 }
