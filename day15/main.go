45fdced0899fccd03bbc811845a51e6939a3c200
Daniele Megna <megna.dany@gmail.com>
2025-08-09 21:15:49 +0200

    Day 15 start: red acceptance test and dummy return implementation

---
+++ day15/main.go
@@ -0,0 +1,5 @@
+package day15
+
+func BoxesGPSCoordinatesSum(fileContent string) int {
+	return 2028
+}
---
+++ day15/main_test.go
@@ -0,0 +1,74 @@
+package day15
+
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"strings"
+	"testing"
+)
+
+var SMALLER_PROVIDED_EXAMPLE_INPUT_LINES = []string{
+	"########",
+	"#..O.O.#",
+	"##@.O..#",
+	"#...O..#",
+	"#.#.O..#",
+	"#...O..#",
+	"#......#",
+	"########",
+	"",
+	"<^^>>>vv<v>>v<<",
+}
+
+var LARGER_PROVIDED_EXAMPLE_INPUT_LINES = []string{
+	"##########",
+	"#..O..O.O#",
+	"#......O.#",
+	"#.OO..O.O#",
+	"#..O@..O.#",
+	"#O#..O...#",
+	"#O..O..O.#",
+	"#.OO.O.OO#",
+	"#....O...#",
+	"##########",
+	"",
+	"<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^",
+	"vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v",
+	"><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<",
+	"<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^",
+	"^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><",
+	"^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^",
+	">^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^",
+	"<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>",
+	"^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>",
+	"v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^",
+}
+
+func TestSolveFirstPartWithSmallerProvidedExample(t *testing.T) {
+	var fileContent = simulateFileContent(SMALLER_PROVIDED_EXAMPLE_INPUT_LINES)
+	assert.Equal(t, 2028, BoxesGPSCoordinatesSum(fileContent))
+}
+
+func TestSolveFirstPartWithLargerProvidedExample(t *testing.T) {
+	t.Skip("WIP")
+	var fileContent = simulateFileContent(LARGER_PROVIDED_EXAMPLE_INPUT_LINES)
+	assert.Equal(t, 10092, BoxesGPSCoordinatesSum(fileContent))
+}
+
+func TestSolveFirstPartWithFile(t *testing.T) {
+	t.Skip("WIP")
+	var fileContent = readFileContent()
+	assert.Equal(t, -1, BoxesGPSCoordinatesSum(fileContent))
+}
+
+func simulateFileContent(inputLines []string) string {
+	return strings.Join(inputLines, "\n") + "\n"
+}
+
+func readFileContent() string {
+	var fileBytes, err = os.ReadFile("input.txt")
+	if err != nil {
+		panic("Cannot find input.txt file to solve with file")
+	}
+	return string(fileBytes)
+}
