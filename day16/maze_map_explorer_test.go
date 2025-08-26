c73dd73dfaddd4cc6ad8203bd2fbe83a14be000c
Daniele Megna <megna.dany@gmail.com>
2025-08-26 08:44:10 +0200

    Starting with MazeMap and related explorer
    created also Coordinate, Direction and Reindeer concepts

---
+++ day16/maze_map_explorer_test.go
@@ -0,0 +1,34 @@
+package day16
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCostOneStepForehand(t *testing.T) {
+	var reindeer = Reindeer{Coordinate: Coordinate{0, 0}, Direction: RIGHT}
+	var target = Coordinate{1, 0}
+	var maze = MazeMap{
+		{EMPTY, EMPTY},
+		{EMPTY, EMPTY},
+	}
+
+	var cost = FindLowestCostToReachTarget(maze, reindeer, target)
+
+	assert.Equal(t, 1, cost)
+}
+func TestCostWithEmptySmallMap(t *testing.T) {
+	t.Skip("WIP")
+	var reindeer = Reindeer{Coordinate: Coordinate{0, 3}, Direction: RIGHT}
+	var target = Coordinate{3, 0}
+	var maze = MazeMap{
+		{EMPTY, EMPTY, EMPTY, EMPTY},
+		{EMPTY, EMPTY, EMPTY, EMPTY},
+		{EMPTY, EMPTY, EMPTY, EMPTY},
+		{EMPTY, EMPTY, EMPTY, EMPTY},
+	}
+
+	var cost = FindLowestCostToReachTarget(maze, reindeer, target)
+
+	assert.Equal(t, 1006, cost)
+}
