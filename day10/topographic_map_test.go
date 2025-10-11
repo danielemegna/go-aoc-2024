89a93a4b424cf15ed1b74c8360924c9b5aba6f6c
Daniele Megna <megna.dany@gmail.com>
2025-10-11 11:20:27 +0200

    Add tests from provided examples
    WIP remove duplicate in reached nine height positions

--- day10/topographic_map_test.go
+++ day10/topographic_map_test.go
@@ -68,3 +68,45 @@ func TestSingleTrailheadStartingBottomRight(t *testing.T) {
 	}}, trailheads)
 	assert.Equal(t, 1, trailheads[0].GetScore())
 }
+
+func TestTwoTrailheadInMap(t *testing.T) {
+	var topographicMap = TopographicMap{
+		{1, 0, 9, 1, 9, 1, 1},
+		{2, 9, 1, 1, 8, 1, 1},
+		{3, 1, 1, 1, 7, 1, 1},
+		{4, 5, 6, 7, 6, 5, 4},
+		{1, 1, 1, 8, 1, 1, 3},
+		{1, 1, 1, 9, 1, 9, 2},
+		{1, 1, 1, 1, 9, 0, 1},
+	}
+
+	var trailheads = topographicMap.FindTrailheads()
+
+	assert.Len(t, trailheads, 2)
+	var firstTrailhead = trailheads[0]
+	assert.Equal(t, Coordinate{1, 0}, firstTrailhead.startingPosition)
+	assert.ElementsMatch(t, []Coordinate{{3, 5}}, firstTrailhead.reachableNineHeightPositions)
+	assert.Equal(t, 1, firstTrailhead.GetScore())
+	var secondTrailhead = trailheads[1]
+	assert.Equal(t, Coordinate{5, 6}, secondTrailhead.startingPosition)
+	assert.ElementsMatch(t, []Coordinate{{3, 5}, {4, 0}}, secondTrailhead.reachableNineHeightPositions)
+	assert.Equal(t, 2, secondTrailhead.GetScore())
+}
+
+func TestSingleTrailheadWithFourReachedNineHeightPositions(t *testing.T) {
+	t.Skip("WIP remove duplicate in reached nine height positions")
+	var topographicMap = TopographicMap{
+		{1, 1, 9, 0, 9, 1, 9},
+		{1, 1, 1, 1, 1, 9, 8},
+		{1, 1, 1, 2, 1, 1, 7},
+		{6, 5, 4, 3, 4, 5, 6},
+		{7, 6, 5, 1, 9, 8, 7},
+		{8, 7, 6, 1, 1, 1, 1},
+		{9, 8, 7, 1, 1, 1, 1},
+	}
+
+	var trailheads = topographicMap.FindTrailheads()
+
+	assert.Len(t, trailheads, 1)
+	assert.Equal(t, 4, trailheads[0].GetScore())
+}
