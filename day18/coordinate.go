03945ea6b83bafc13a1635862f177daa0472c43e
Daniele Megna <megna.dany@gmail.com>
2025-10-22 08:07:58 +0200

    Introduce Coordinate and some util functions for MemorySpace

---
+++ day18/coordinate.go
@@ -0,0 +1,15 @@
+package day18
+
+type Coordinate struct {
+	X int
+	Y int
+}
+
+func (c Coordinate) CloseCoordinates() []Coordinate {
+	return []Coordinate{
+		{c.X - 1, c.Y},
+		{c.X, c.Y - 1},
+		{c.X + 1, c.Y},
+		{c.X, c.Y + 1},
+	}
+}
--- day18/memory_space.go
+++ day18/memory_space.go
@@ -1,7 +1,5 @@
 package day18
 
-type MemorySpace [][]MemoryState
-
 type MemoryState int
 
 const (
@@ -9,6 +7,20 @@ const (
 	CORRUPTED
 )
 
+type MemorySpace [][]MemoryState
+
 func (this MemorySpace) ShortestPathLengthFromTopLeftToBottomRightCorners() int {
 	return 4
+}
+
+func (this MemorySpace) StateAt(c Coordinate) MemoryState {
+	return this[c.Y][c.X]
+}
+
+func (this MemorySpace) IsOutOfBounds(c Coordinate) bool {
+	return c.X < 0 || c.Y < 0 || c.Y >= len(this) || c.X >= len(this[c.Y])
+}
+
+func (this MemorySpace) IsVisitable(c Coordinate) bool {
+	return !this.IsOutOfBounds(c) && this.StateAt(c) == SAFE
 }
