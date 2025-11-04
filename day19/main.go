e8527323420fdccd94067a030d4bc5b9f6af2f0a
Daniele Megna <megna.dany@gmail.com>
2025-11-04 08:32:49 +0100

    Integrate TowelPattern and AvailableTowelPatterns in main
    provided example acceptance passes :ok:

--- day19/main.go
+++ day19/main.go
@@ -1,5 +1,23 @@
 package day19
 
+import "strings"
+
 func CountPossibleDesigns(fileContent string) int {
-	return 6
+	var fileContentLines = linesFrom(fileContent)
+	var availableTowelPatterns = InitAvailableTowelPatternsFrom(fileContentLines[0])
+
+	var possibleDesigns = 0
+	for _, towelStringPattern := range fileContentLines[2:] {
+		var towelPattern = TowelPattern(towelStringPattern)
+		if towelPattern.IsDesignPossibleWith(availableTowelPatterns) {
+			possibleDesigns++
+		}
+	}
+
+	return possibleDesigns
+}
+
+func linesFrom(s string) []string {
+	var lines = strings.Split(s, "\n")
+	return lines[:len(lines)-1]
 }
