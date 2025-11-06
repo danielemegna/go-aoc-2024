027846b76e7a802842c6a2af9103e7ffe92851bd
Daniele Megna <megna.dany@gmail.com>
2025-11-06 09:42:24 +0100

    Starting PossibleDesignCombinationsFor evolution ..

--- day19/available_towel_patterns.go
+++ day19/available_towel_patterns.go
@@ -9,13 +9,15 @@ type TowelPattern = string
 type ColorByte = byte
 
 type AvailableTowelPatterns struct {
-	regex *regexp.Regexp
+	regex    *regexp.Regexp
+	patterns []string
 }
 
 func AvailableTowelPatternsFrom(raw string) AvailableTowelPatterns {
 	var stringRegexp = "^(" + strings.ReplaceAll(raw, ", ", "|") + ")+$"
 	var regexp = regexp.MustCompile(stringRegexp)
-	return AvailableTowelPatterns{regexp}
+	var patterns = strings.Split(raw, ", ")
+	return AvailableTowelPatterns{regexp, patterns}
 }
 
 func (this AvailableTowelPatterns) IsDesignPossible(towelPattern TowelPattern) bool {
@@ -24,9 +26,9 @@ func (this AvailableTowelPatterns) IsDesignPossible(towelPattern TowelPattern) b
 }
 
 func (this AvailableTowelPatterns) PossibleDesignCombinationsFor(towelPattern TowelPattern) int {
-	if(this.IsDesignPossible(towelPattern)) {
-		return 1
+	if !this.IsDesignPossible(towelPattern) {
+		return 0
 	}
 
-	return 0
+	return 1
 }
