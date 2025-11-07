d9859ac1698c35317c7eaf9b37d4e88dec45d7fe
Daniele Megna <megna.dany@gmail.com>
2025-11-07 07:47:44 +0100

    Restore patterns as map[ColorByte][]TowelPattern in AvailableTowelPatterns
    it keeps possible towel patterns organized by first color
    and sorted by length DESC

--- day19/available_towel_patterns.go
+++ day19/available_towel_patterns.go
@@ -2,6 +2,7 @@ package day19
 
 import (
 	"regexp"
+	"slices"
 	"strings"
 )
 
@@ -10,14 +11,18 @@ type ColorByte = byte
 
 type AvailableTowelPatterns struct {
 	regex    *regexp.Regexp
-	patterns []string
+	patterns map[ColorByte][]TowelPattern
 }
 
 func AvailableTowelPatternsFrom(raw string) AvailableTowelPatterns {
 	var stringRegexp = "^(" + strings.ReplaceAll(raw, ", ", "|") + ")+$"
 	var regexp = regexp.MustCompile(stringRegexp)
-	var patterns = strings.Split(raw, ", ")
-	return AvailableTowelPatterns{regexp, patterns}
+	var patterns = AvailableTowelPatterns{regexp, map[ColorByte][]string{}}
+	for pattern := range strings.SplitSeq(raw, ", ") {
+		patterns.add(pattern)
+	}
+
+	return patterns
 }
 
 func (this AvailableTowelPatterns) IsDesignPossible(towelPattern TowelPattern) bool {
@@ -31,4 +36,21 @@ func (this AvailableTowelPatterns) PossibleDesignCombinationsFor(towelPattern To
 	}
 
 	return 1
+}
+
+func (this *AvailableTowelPatterns) add(rawStringPattern string) {
+	var firstColorByte = rawStringPattern[0]
+	var existingSlice, keyExists = this.patterns[firstColorByte]
+	if !keyExists {
+		this.patterns[firstColorByte] = []string{rawStringPattern}
+		return
+	}
+
+	for index, existingElement := range existingSlice {
+		if len(rawStringPattern) >= len(existingElement) {
+			this.patterns[firstColorByte] = slices.Insert(existingSlice, index, rawStringPattern)
+			return
+		}
+	}
+	this.patterns[firstColorByte] = append(existingSlice, rawStringPattern)
 }
