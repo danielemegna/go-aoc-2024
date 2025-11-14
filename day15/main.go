5ec81c4692b5f41b7d6b9eeaffa5deaa19cdeb70
Daniele Megna <megna.dany@gmail.com>
2025-11-14 15:11:30 +0100

    [AIexp] delete day15 solution to see an AI solution

--- day15/main.go
+++ day15/main.go
@@ -1,21 +1,6 @@
 package day15
 
 func BoxesGPSCoordinatesSumAfterAllRobotMoves(fileContent string) int {
-	var warehouseMap, robotMoves = ParseWarehouseMapAndMoves(fileContent)
-
-	for _, move := range robotMoves {
-		warehouseMap.MoveRobot(move)
-	}
-
-	return warehouseMap.GetBoxesGPSCoordinatesSum()
+	return 2028
 }
 
-func BoxesGPSCoordinatesSumAfterAllRobotMovesInDoubleWide(fileContent string) int {
-	var warehouseMap, robotMoves = ParseWarehouseMapAndMovesInDoubleWide(fileContent)
-
-	for _, move := range robotMoves {
-		warehouseMap.MoveRobot(move)
-	}
-
-	return warehouseMap.GetBoxesGPSCoordinatesSum()
-}
