5b767c7f053c40cd6a49bf2e9ed281fc8117f760
Daniele Megna <megna.dany@gmail.com>
2025-11-17 07:28:17 +0100

    [day08] Improve CityMap tests

--- day08/city_map_test.go
+++ day08/city_map_test.go
@@ -5,28 +5,15 @@ import (
 	"testing"
 )
 
-var EMPTY_CITY_MAP_EXAMPLE_INPUT_LINES = []string{
-	"......",
-	"......",
-	"......",
-	"......",
-	"......",
-	"......",
-}
-
-var SIMPLE_CITY_MAP_EXAMPLE_INPUT_LINES = []string{
-	"........",
-	"...A....",
-	"..A.....",
-	"........",
-	"....B...",
-	"........",
-	"....B...",
-	"........",
-}
-
 func TestParseAndInspectEmptyCityMap(t *testing.T) {
-	var cityMap = ParseCityMap(simulateFileContent(EMPTY_CITY_MAP_EXAMPLE_INPUT_LINES))
+	var cityMap = ParseCityMap(simulateFileContent([]string{
+		"......",
+		"......",
+		"......",
+		"......",
+		"......",
+		"......",
+	}))
 
 	assert.Equal(t, 6, cityMap.Size())
 	assert.Equal(t, []Coordinate{}, cityMap.Antennas())
@@ -34,11 +21,20 @@ func TestParseAndInspectEmptyCityMap(t *testing.T) {
 }
 
 func TestParseAndInspectSimpleCityMap(t *testing.T) {
-	var cityMap = ParseCityMap(simulateFileContent(SIMPLE_CITY_MAP_EXAMPLE_INPUT_LINES))
+	var cityMap = ParseCityMap(simulateFileContent([]string{
+		"........",
+		"...A....",
+		"..A.....",
+		"......B.",
+		"........",
+		"......B.",
+		"........",
+		"........",
+	}))
 
 	assert.Equal(t, 8, cityMap.Size())
-	assert.ElementsMatch(t, []Coordinate{{3, 1}, {2, 2}, {4, 4}, {4, 6}}, cityMap.Antennas())
-	assert.Len(t, cityMap.AntinodesInMap(), 4)
+	assert.ElementsMatch(t, []Coordinate{{3, 1}, {2, 2}, {6, 3}, {6, 5}}, cityMap.Antennas())
+	assert.ElementsMatch(t, []Coordinate{{4, 0}, {1, 3}, {6, 1}, {6, 7}}, cityMap.AntinodesInMap())
 }
 
 func TestParseAndInspectProvidedExampleMap(t *testing.T) {
@@ -46,6 +42,6 @@ func TestParseAndInspectProvidedExampleMap(t *testing.T) {
 
 	assert.Equal(t, 12, cityMap.Size())
 	assert.ElementsMatch(t, []Coordinate{{8, 1}, {5, 2}, {7, 3}, {4, 4}, {6, 5}, {8, 8}, {9, 9}}, cityMap.Antennas())
-	// TODO exclude out of bound antinodes
-	// assert.Len(t, cityMap.AntinodesInMap(), 13)
+	// TODO exclude duplicated and out of bound antinodes
+	// assert.Len(t, cityMap.AntinodesInMap(), 13+1)
 }
