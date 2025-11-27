cf021f8fddf0da5582d3a69ad86f7188bcafccba
Daniele Megna <megna.dany@gmail.com>
2025-11-27 11:38:59 +0100

    [day20] Starting day 20 with acceptance test and dummy implementation

---
+++ day20/main.go
@@ -0,0 +1,6 @@
+package day20
+
+
+func CountCheatsSavingAtLeast(picoseconds int, fileContent string) int {
+	return 10
+}
---
+++ day20/main_test.go
@@ -0,0 +1,49 @@
+package day20
+
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"strings"
+	"testing"
+)
+
+var PROVIDED_EXAMPLE_INPUT_LINES = []string{
+	"###############",
+	"#...#...#.....#",
+	"#.#.#.#.#.###.#",
+	"#S#...#.#.#...#",
+	"#######.#.#.###",
+	"#######.#.#...#",
+	"#######.#.###.#",
+	"###..E#...#...#",
+	"###.#######.###",
+	"#...###...#...#",
+	"#.#####.#.###.#",
+	"#.#...#.#.#...#",
+	"#.#.#.#.#.#.###",
+	"#...#...#...###",
+	"###############",
+}
+
+func TestSolveFirstPartWithProvidedExample(t *testing.T) {
+	var fileContent = simulateFileContent(PROVIDED_EXAMPLE_INPUT_LINES)
+	assert.Equal(t, 2 + 3 + 1 + 1 + 1 + 1 + 1, CountCheatsSavingAtLeast(10, fileContent))
+}
+
+func TestSolveFirstPartWithFile(t *testing.T) {
+	t.Skip("WIP")
+	var fileContent = readFileContent()
+	assert.Equal(t, -1, CountCheatsSavingAtLeast(100, fileContent))
+}
+
+func simulateFileContent(inputLines []string) string {
+	return strings.Join(inputLines, "\n") + "\n"
+}
+
+func readFileContent() string {
+	var fileBytes, err = os.ReadFile("input.txt")
+	if err != nil {
+		panic("Cannot find input.txt file to solve with file")
+	}
+	return string(fileBytes)
+}
