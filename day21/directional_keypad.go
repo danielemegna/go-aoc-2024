252ef50b75cf83adfe696210c1bf0bc891728256
Daniele Megna <megna.dany@gmail.com>
2025-12-17 08:33:30 +0100

    Modelling DirectionalKeypad

---
+++ day21/directional_keypad.go
@@ -0,0 +1,89 @@
+package day21
+
+type DirectionalKeypad struct {
+	position DirectionalKeypadButton
+}
+
+func (this DirectionalKeypad) MovesToReach(positionToReach DirectionalKeypadButton) []Move {
+	switch this.position {
+	case ACTIVATE:
+		return movesToReachFromActivate(positionToReach)
+	case UP:
+		return movesToReachFromUp(positionToReach)
+	case DOWN:
+		return movesToReachFromDown(positionToReach)
+	case LEFT:
+		return movesToReachFromLeft(positionToReach)
+	case RIGHT:
+		return movesToReachFromRight(positionToReach)
+	default:
+		return []Move{}
+	}
+}
+
+func movesToReachFromUp(positionToReach DirectionalKeypadButton) []Move {
+	switch positionToReach {
+	case UP:
+		return []Move{}
+	case ACTIVATE:
+		return []Move{RIGHT}
+	default:
+		return append([]Move{DOWN}, movesToReachFromDown(positionToReach)...)
+	}
+}
+
+func movesToReachFromRight(positionToReach DirectionalKeypadButton) []Move {
+	switch positionToReach {
+	case RIGHT:
+		return []Move{}
+	case ACTIVATE:
+		return []Move{UP}
+	default:
+		return append([]Move{LEFT}, movesToReachFromDown(positionToReach)...)
+	}
+}
+
+func movesToReachFromActivate(positionToReach DirectionalKeypadButton) []Move {
+	switch positionToReach {
+	case ACTIVATE:
+		return []Move{}
+	case RIGHT:
+		return []Move{DOWN}
+	default:
+		return append([]Move{LEFT}, movesToReachFromUp(positionToReach)...)
+	}
+}
+
+func movesToReachFromDown(positionToReach DirectionalKeypadButton) []Move {
+	switch positionToReach {
+	case DOWN:
+		return []Move{}
+	case RIGHT:
+		return []Move{RIGHT}
+	case LEFT:
+		return []Move{LEFT}
+	default:
+		return append([]Move{UP}, movesToReachFromUp(positionToReach)...)
+	}
+}
+
+func movesToReachFromLeft(positionToReach DirectionalKeypadButton) []Move {
+	switch positionToReach {
+	case LEFT:
+		return []Move{}
+	default:
+		return append([]Move{RIGHT}, movesToReachFromDown(positionToReach)...)
+	}
+}
+
+type Move int
+
+const (
+	ACTIVATE Move = iota
+	LEFT
+	RIGHT
+	DOWN
+	UP
+)
+
+type DirectionalKeypadButton = Move
---
+++ day21/directional_keypad_test.go
@@ -0,0 +1,46 @@
+package day21
+
+import (
+	"github.com/stretchr/testify/assert"
+	"strconv"
+	"testing"
+)
+
+func TestMovesToReach(t *testing.T) {
+	var testCases = []struct {
+		startingPosition DirectionalKeypadButton
+		positionToReach  DirectionalKeypadButton
+		expectedMoves    []Move
+	}{
+		{startingPosition: ACTIVATE, positionToReach: UP, expectedMoves: []Move{LEFT}},
+		{startingPosition: ACTIVATE, positionToReach: RIGHT, expectedMoves: []Move{DOWN}},
+		{startingPosition: ACTIVATE, positionToReach: DOWN, expectedMoves: []Move{LEFT, DOWN}},
+		{startingPosition: ACTIVATE, positionToReach: LEFT, expectedMoves: []Move{LEFT, DOWN, LEFT}},
+		{startingPosition: ACTIVATE, positionToReach: ACTIVATE, expectedMoves: []Move{}},
+		{startingPosition: DOWN, positionToReach: LEFT, expectedMoves: []Move{LEFT}},
+		{startingPosition: DOWN, positionToReach: UP, expectedMoves: []Move{UP}},
+		{startingPosition: DOWN, positionToReach: ACTIVATE, expectedMoves: []Move{UP, RIGHT}},
+		{startingPosition: DOWN, positionToReach: DOWN, expectedMoves: []Move{}},
+		{startingPosition: LEFT, positionToReach: DOWN, expectedMoves: []Move{RIGHT}},
+		{startingPosition: LEFT, positionToReach: UP, expectedMoves: []Move{RIGHT, UP}},
+		{startingPosition: LEFT, positionToReach: ACTIVATE, expectedMoves: []Move{RIGHT, UP, RIGHT}},
+		{startingPosition: LEFT, positionToReach: RIGHT, expectedMoves: []Move{RIGHT, RIGHT}},
+		{startingPosition: RIGHT, positionToReach: ACTIVATE, expectedMoves: []Move{UP}},
+		{startingPosition: RIGHT, positionToReach: UP, expectedMoves: []Move{LEFT, UP}},
+		{startingPosition: RIGHT, positionToReach: LEFT, expectedMoves: []Move{LEFT, LEFT}},
+		{startingPosition: RIGHT, positionToReach: RIGHT, expectedMoves: []Move{}},
+		{startingPosition: UP, positionToReach: ACTIVATE, expectedMoves: []Move{RIGHT}},
+		{startingPosition: UP, positionToReach: DOWN, expectedMoves: []Move{DOWN}},
+		{startingPosition: UP, positionToReach: LEFT, expectedMoves: []Move{DOWN, LEFT}},
+		{startingPosition: UP, positionToReach: RIGHT, expectedMoves: []Move{DOWN, RIGHT}},
+	}
+
+	for index, testCase := range testCases {
+		t.Run("Test case #"+strconv.Itoa(index+1), func(t *testing.T) {
+			var keypad = DirectionalKeypad{position: testCase.startingPosition}
+			var moves = keypad.MovesToReach(testCase.positionToReach)
+			assert.Equal(t, testCase.expectedMoves, moves)
+		})
+	}
+
+}
