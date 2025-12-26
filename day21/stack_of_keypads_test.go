29374b7c5f6e187edb7fafb10800357afe3fe614
Daniele Megna <megna.dany@gmail.com>
2025-12-26 17:12:29 +0100

    Add more test cases to StackOfKeypads
    some particular code sequence takes very long to solve and
    seems to use lot of RAM resource ... i am suspecting
    a non-optimized tail recursion here

--- day21/stack_of_keypads_test.go
+++ day21/stack_of_keypads_test.go
@@ -35,6 +35,10 @@ func TestComposeCodeWithStackOfFourKeypads(t *testing.T) {
 		{codeToCompose: []NumericKeypadButton{1, 7, 9, ACT}, expectedLength: 68},
 		{codeToCompose: []NumericKeypadButton{4, 5, 6, ACT}, expectedLength: 64},
 		{codeToCompose: []NumericKeypadButton{3, 7, 9, ACT}, expectedLength: 64},
+		{codeToCompose: []NumericKeypadButton{5, 2, 8, ACT}, expectedLength: 70},
+		{codeToCompose: []NumericKeypadButton{5, 8, 6, ACT}, expectedLength: 68},
+		{codeToCompose: []NumericKeypadButton{3, 4, 1, ACT}, expectedLength: 72},
+		{codeToCompose: []NumericKeypadButton{3, 1, 9, ACT}, expectedLength: 70},
 	}
 
 	var stack = StackOfKeypads{totalNumberOfKeypads: 4}
@@ -44,4 +48,15 @@ func TestComposeCodeWithStackOfFourKeypads(t *testing.T) {
 			assert.Equal(t, testCase.expectedLength, actual)
 		})
 	}
+}
+
+// this is taking more than 3s using around 2GB of ram (!) to solve
+// (non-optimized recursion maybe?)
+func TestComposeSpecialCaseCodeWithStackOfFourKeypads_takesLotOfResources(t *testing.T) {
+	var codeToCompose = []NumericKeypadButton{8, 0, 3, ACT}
+	var stack = StackOfKeypads{totalNumberOfKeypads: 4}
+
+	var actual = stack.LengthOfShortestSequenceToPressOnFirstKeypadFor(codeToCompose)
+
+	assert.Equal(t, 76, actual)
 }
